aaf2e7e260a0cff057564f1a51bd96669c91d593
Soeun Sopanhariem <potatocommercial@gmail.com>
2026-04-06 20:55:46 +0700

    Revert "finished book-detail"

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,6 @@ const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/authRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
-const bookRoutes = require("./routes/bookRoutes");
 
 const app = express();
 app.use(
@@ -20,7 +19,6 @@ app.use(cookieParser());
 // Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/transactions", transactionRoutes);
-app.use("/api/v1/books", bookRoutes);
 
 // Health check
 app.get("/api/health", (req, res) => {
